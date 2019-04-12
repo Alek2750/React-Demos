9813163a3104f0efa821ddc32fc54bb5bfa80e4b
Alek <alek2750@gmail.com>
2019-04-12 14:21:03 +0200

    start af sp8 v2

--- sp8v2/src/App.js
+++ sp8v2/src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Router, Route, Link, NavLink, Switch } from "react-router-dom";
+
 import './App.css';
+
+function Header() {
+  return (
+    <ul className="header">
+      <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+      <li><NavLink activeClassName="active" to="/products">Products</NavLink></li>
+      <li><NavLink activeClassName="active" to="/company">Company</NavLink></li>
+    </ul>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello from Home</h1>
+    </div>
+  )
+}
+class Product extends Component {
+  render() {
+    return (
+      <h1>Hello from Product</h1>
+    );
+  }
+};
+
+
+function Company() {
+  return (
+    <div>
+      <h1>Hello from Company</h1>
+    </div>
+  )
+}
+
+function NoMatch() {
+return(
+  <div>
+    <h1>
+      NoMatch found
+    </h1>
+  </div>
+)
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <BrowserRouter>
+      <div>
+        <Header />
+      <Switch >
+          <Route exact path="/" component={Home} />
+          <Route path="/products" component={Product} />
+          <Route path="/company" component={Company} />
+          <Route component={NoMatch}/>
+      </Switch>
       </div>
+      </BrowserRouter>
     );
   }
 }
+
 
 export default App;
